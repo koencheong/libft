5768e91c05996eb6269ea02fb4cba30404c40570
koencheong <koencheong@gmail.com>
2022-05-24 23:38:43 +0800

    Added bzero, memcpy, tolower, toupper

---
+++ ft_memcpy.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kcheong <kcheong@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/24 18:22:32 by kcheong           #+#    #+#             */
+/*   Updated: 2022/05/24 18:22:32 by kcheong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdio.h>
+
+void *ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((char *)dest)[i] = ((char *)src)[i];
+		i++;
+	}
+	return (dest);
+}
+
+
+int	main()
+{
+	char dest[50] = "Hello";
+	const char src[50] = "Byebye";
+	/*strcpy(dest,"Heloooo!!");
+	printf("%s\n", dest);
+	memcpy(dest, src, strlen(src)+1);
+	printf("%s\n", dest);*/
+	printf("%s\n", dest);
+	ft_memcpy(dest, src, strlen(src)+1);
+	printf("%s\n", dest);
+}
