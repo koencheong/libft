343a2aaacbc0377d5fc46ea9e98681527643b03a
Ko en Cheong <kcheong@u91z08s04.42kl.edu.my>
2022-05-25 17:00:51 +0800

    first commit

---
+++ ft_strlcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kcheong <kcheong@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/23 16:08:06 by kcheong           #+#    #+#             */
+/*   Updated: 2022/05/25 16:42:33 by kcheong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	//size_t	srclen;
+	
+	i = 0;
+	//srclen = ft_strlen(src);
+	while (src[i] && i < dstsize - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (0);
+}
+
