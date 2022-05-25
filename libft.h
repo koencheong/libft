196793c7add1e8106f369f5aad5c7194c9629b68
Ko en Cheong <kcheong@u91z08s04.42kl.edu.my>
2022-05-25 17:04:45 +0800

    Second commit

---
+++ libft.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kcheong <kcheong@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/23 11:10:20 by kcheong           #+#    #+#             */
+/*   Updated: 2022/05/25 16:42:27 by kcheong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+void	*ft_memset(void *b, int c, size_t len);
+int		ft_strlen(char *str);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+
+#endif
