0acf87346f92c9b5fec372978039913c86a43c83
koencheong <koencheong@gmail.com>
2022-05-30 15:48:59 +0800

    Forth commit from laptop

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,16 +6,24 @@
 /*   By: kcheong <kcheong@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 14:43:27 by kcheong           #+#    #+#             */
-/*   Updated: 2022/05/23 14:56:16 by kcheong          ###   ########.fr       */
+/*   Updated: 2022/05/30 15:43:02 by kcheong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *str)
+#include "libft.h"
+
+size_t	ft_strlen(const char *s)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
-	while (str[i])
+	while (s[i])
 		i++;
 	return (i);
 }
+
+// int	main()
+// {
+// 	const char s[] = "Hello";
+// 	printf("%li\n", ft_strlen(s));
+// }
--- libft.h
+++ libft.h
@@ -6,13 +6,14 @@
 /*   By: kcheong <kcheong@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 11:10:20 by kcheong           #+#    #+#             */
-/*   Updated: 2022/05/30 13:14:28 by kcheong          ###   ########.fr       */
+/*   Updated: 2022/05/30 15:40:23 by kcheong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+#include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -23,9 +24,13 @@ int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
 void	*ft_memset(void *b, int c, size_t len);
-int		ft_strlen(char *str);
+size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 void	*ft_memmove(void *dest, const void *src, size_t n);
-void *ft_memcpy(void *dest, const void *src, size_t n);
+void 	*ft_memcpy(void *dest, const void *src, size_t n);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+void	ft_bzero(void *s, size_t n);
+char	*ft_strchr(const char *s, int c);
+char	*strrchr(const char *s, int c);
 
 #endif
