166228587b4e278a7343c809a233011163079efd
Ko en Cheong <kcheong@u90z04s03.42kl.edu.my>
2022-06-04 22:32:22 +0800

    Edited Makefile and libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kcheong <kcheong@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 11:10:20 by kcheong           #+#    #+#             */
-/*   Updated: 2022/06/04 18:15:01 by kcheong          ###   ########.fr       */
+/*   Updated: 2022/06/04 22:29:48 by kcheong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,5 +99,7 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
 
 #endif
