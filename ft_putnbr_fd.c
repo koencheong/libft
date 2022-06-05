c079e202281d32aa7b1ab336f370a07d40c21b77
koencheong <63862171+koencheong@users.noreply.github.com>
2022-06-06 00:43:52 +0800

    Update ft_putnbr_fd (manually)

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -29,3 +29,22 @@ void	ft_putnbr_fd(int n, int fd)
 // 	system("leaks putnbr_test.out");
 // 	return (0);
 // }
+
+// void	ft_putnbr_fd(int n, int fd)
+// {
+// 	if (n < 0)
+// 	{
+// 		ft_putchar_fd('-', fd);
+// 		if (n == -2147483648)
+// 		{
+// 			ft_putchar_fd('2', fd)
+// 			n = -147483648;
+// 		}
+// 		n = -n;
+// 	}
+// 	while (n > 0)
+// 	{
+// 		ft_putnbr_fd(n / 10, fd);
+// 		ft_putchar_fd((n % 10) + '0', fd);
+// 	}
+// }
